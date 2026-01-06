689e9012fc4bfe9f5b2cadfcbc3a4c11c2444ee8
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2026-01-06 20:38:00 -0300

    feat(133): Criando teste int. valid. cors
    Closes#cors

--- src/test/java/br/com/cristianoaf81/config/TestConfigs.java
+++ src/test/java/br/com/cristianoaf81/config/TestConfigs.java
@@ -2,4 +2,11 @@ package br.com.cristianoaf81.config;
 
 public interface TestConfigs {
   int SERVER_PORT = 8888;
+  String HEADER_PARAM_AUTHORIZATION = "Authorization";
+  String HEADER_PARAM_ORIGIN = "Origin";
+  String ORIGIN_GOOGLE = "https://www.google.com.br";
+  String ORIGIN_GOOGLE_HTTP = "http://www.google.com.br";
+  String ORIGIN_LOCALHOST = "http://localhost:8080";
+  String ORIGIN_LOCALHOST_8888 = "http://localhost:8888";
+  String ORIGIN_LOCALHOST_3000 = "http://localhost:3000";
 }
---
+++ src/test/java/br/com/cristianoaf81/integrationtests/controllers/withjson/PersonControllerTest.java
@@ -0,0 +1,110 @@
+package br.com.cristianoaf81.integrationtests.controllers.withjson;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import br.com.cristianoaf81.config.TestConfigs;
+import br.com.cristianoaf81.dto.v1.PersonDTO;
+import br.com.cristianoaf81.integrationtests.testcontainers.AbstractIntegrationTest;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+  properties = {
+    "server.port=8888",
+    "cors.originPatterns: http://localhost:8080,https://www.google.com.br,http://localhost:3000,http://www.google.com.br"
+  }
+)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+public class PersonControllerTest extends AbstractIntegrationTest {
+  
+  private static RequestSpecification specification;
+  private static ObjectMapper objectMapper;
+  private static PersonDTO person;
+
+  @BeforeEach
+  void setup() {
+    objectMapper = new ObjectMapper();
+    objectMapper.disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    person = new PersonDTO();
+  }
+
+  @Test
+  @Order(1)
+  void create() throws JsonProcessingException {
+    mockPerson();
+    specification = new RequestSpecBuilder()
+    .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_GOOGLE)
+    .setBasePath("/api/person/v1")
+    .setPort(TestConfigs.SERVER_PORT)
+    .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+    .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+    .build();
+
+    var content = given(specification)
+    .contentType(MediaType.APPLICATION_JSON_VALUE)
+    .body(person)
+    .when()
+    .post()
+    .then()
+    .statusCode(200)
+    .extract()
+    .body()
+    .asString();
+
+    PersonDTO createdPerson = objectMapper.readValue(content, PersonDTO.class);
+    person = createdPerson;
+
+    assertNotNull(createdPerson.getId());
+    assertTrue(createdPerson.getId() > 0);
+    assertNotNull(createdPerson.getFirstName());
+    assertNotNull(createdPerson.getLastName());
+    assertNotNull(createdPerson.getAddress());
+    assertNotNull(createdPerson.getGender());
+
+
+    assertEquals("Richard",createdPerson.getFirstName());
+    assertEquals("Stall",createdPerson.getLastName());
+    assertEquals("New York City - EUA",createdPerson.getAddress());
+    assertEquals("Male",createdPerson.getGender());
+ }
+
+  @Test
+  void findById() {}
+
+  @Test
+  void update() {}
+
+  @Test
+  void delete() {}
+
+  @Test
+  void findAll() {}
+
+  private void mockPerson() {
+    person.setFirstName("Richard");
+    person.setLastName("Stall");
+    person.setAddress("New York City - EUA");
+    person.setGender("Male");
+  }
+}
