62f70bfa2357476387b8c15949e2bf2650b0e224
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2026-02-09 13:57:38 -0300

    feat(146): finalizando testes integracao com yaml
      adicionando configuracao de deserializacao no metodo given
      corrigindo o metodo de deserializacao de YamlMapper
    
    Closes#verbo-patch

---
+++ src/test/java/br/com/cristianoaf81/integrationtests/controllers/withyaml/PersonControllerYmlTest.java
@@ -0,0 +1,316 @@
+package br.com.cristianoaf81.integrationtests.controllers.withyaml;
+
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.jupiter.api.MethodOrderer;
+import org.junit.jupiter.api.Order;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestMethodOrder;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+
+import br.com.cristianoaf81.config.TestConfigs;
+import br.com.cristianoaf81.dto.v1.PersonDTO;
+import br.com.cristianoaf81.integrationtests.controllers.withyaml.mapper.YamlMapper;
+import br.com.cristianoaf81.integrationtests.testcontainers.AbstractIntegrationTest;
+import io.restassured.RestAssured;
+import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.config.EncoderConfig;
+import io.restassured.filter.log.LogDetail;
+import io.restassured.filter.log.RequestLoggingFilter;
+import io.restassured.filter.log.ResponseLoggingFilter;
+import io.restassured.http.ContentType;
+import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.BeforeAll;
+
+
+@SpringBootTest(
+  webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT,
+  properties = {
+    "server.port=8888",
+    "cors.originPatterns: http://localhost:8080,https://www.google.com.br,http://localhost:3000,http://www.google.com.br"
+  }
+)
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@ActiveProfiles("test")
+public class PersonControllerYmlTest extends AbstractIntegrationTest {
+  
+  private static RequestSpecification specification;
+  private static YamlMapper objectMapper;
+  private static PersonDTO person;
+
+  @BeforeAll
+  static void setup() {
+    objectMapper = new YamlMapper();
+    person = new PersonDTO();
+  }
+
+  @Test
+  @Order(1)
+  void create() throws JsonProcessingException {
+    mockPerson();
+    person.setLastName("Benedict Torvalds");
+    specification = new RequestSpecBuilder()
+    .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_GOOGLE)
+    .setBasePath("/api/person/v1")
+    .setPort(TestConfigs.SERVER_PORT)
+    .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+    .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+    .build();
+
+    var createdPerson = given().config(
+      RestAssured.config()
+      .encoderConfig(
+        EncoderConfig
+        .encoderConfig()
+        .encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT)
+      )
+    ).spec(specification)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)    
+    .accept(MediaType.APPLICATION_YAML_VALUE)
+    .body(person, objectMapper)
+    .when()
+    .post()
+    .then()
+    .statusCode(200)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)
+    .extract()
+    .body()
+    .as(PersonDTO.class, objectMapper);
+
+    person = createdPerson;
+
+    assertNotNull(createdPerson.getId());
+    assertTrue(createdPerson.getId() > 0);
+    assertNotNull(createdPerson.getFirstName());
+    assertNotNull(createdPerson.getLastName());
+    assertNotNull(createdPerson.getAddress());
+    assertNotNull(createdPerson.getGender());
+
+
+    assertEquals("Linus",createdPerson.getFirstName());
+    assertEquals("Benedict Torvalds",createdPerson.getLastName());
+    assertEquals("Helsink - FINLAND",createdPerson.getAddress());
+    assertEquals("Male",createdPerson.getGender());
+    assertTrue(createdPerson.getEnabled());
+  }
+
+  @Test
+  @Order(2)
+  void findById() throws JsonProcessingException {
+    var createdPerson = given().config(
+      RestAssured.config()
+      .encoderConfig(
+        EncoderConfig
+        .encoderConfig()
+        .encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT)
+      )
+    ).spec(specification)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)    
+    .accept(MediaType.APPLICATION_YAML_VALUE)
+    .pathParam("id", person.getId())
+    .when()
+    .get("{id}")
+    .then()
+    .statusCode(200)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)
+    .extract()
+    .body()
+    .as(PersonDTO.class, objectMapper);
+
+    person = createdPerson;
+
+    assertNotNull(createdPerson.getId());
+    assertTrue(createdPerson.getId() > 0);
+    assertNotNull(createdPerson.getFirstName());
+    assertNotNull(createdPerson.getLastName());
+    assertNotNull(createdPerson.getAddress());
+    assertNotNull(createdPerson.getGender());
+    assertTrue(createdPerson.getEnabled());
+
+    assertEquals("Linus",createdPerson.getFirstName());
+    assertEquals("Benedict Torvalds",createdPerson.getLastName());
+    assertEquals("Helsink - FINLAND",createdPerson.getAddress());
+    assertEquals("Male",createdPerson.getGender());
+    assertTrue(createdPerson.getEnabled());
+  }
+
+  @Test
+  @Order(3)
+  void update() throws JsonProcessingException {
+    mockPerson();
+    specification = new RequestSpecBuilder()
+    .addHeader(TestConfigs.HEADER_PARAM_ORIGIN, TestConfigs.ORIGIN_GOOGLE)
+    .setBasePath("/api/person/v1")
+    .setPort(TestConfigs.SERVER_PORT)
+    .addFilter(new RequestLoggingFilter(LogDetail.ALL))
+    .addFilter(new ResponseLoggingFilter(LogDetail.ALL))
+    .build();
+
+    var createdPerson = given().config(
+      RestAssured.config()
+      .encoderConfig(
+        EncoderConfig
+        .encoderConfig()
+        .encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT)
+      )
+    ).spec(specification)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)
+    .accept(MediaType.APPLICATION_YAML_VALUE)
+    .body(person, objectMapper)
+    .when()
+    .put()
+    .then()
+    .statusCode(200)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)
+    .extract()
+    .body()
+    .as(PersonDTO.class, objectMapper);
+
+    person = createdPerson;
+
+    assertNotNull(createdPerson.getId());
+    assertTrue(createdPerson.getId() > 0);
+    assertNotNull(createdPerson.getFirstName());
+    assertNotNull(createdPerson.getLastName());
+    assertNotNull(createdPerson.getAddress());
+    assertNotNull(createdPerson.getGender());
+
+
+    assertEquals("Linus",createdPerson.getFirstName());
+    assertEquals("Torvalds",createdPerson.getLastName());
+    assertEquals("Helsink - FINLAND",createdPerson.getAddress());
+    assertEquals("Male",createdPerson.getGender());
+    assertTrue(createdPerson.getEnabled());
+
+  }
+
+
+  @Test
+  @Order(4)
+  void disable() throws JsonProcessingException {
+    var createdPerson = given().config(
+      RestAssured.config()
+      .encoderConfig(
+        EncoderConfig
+        .encoderConfig()
+        .encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT)
+      )
+    ).spec(specification)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)
+    .accept(MediaType.APPLICATION_YAML_VALUE)
+    .pathParam("id", person.getId())
+    .when()
+    .patch("{id}")
+    .then()
+    .statusCode(200)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)
+    .extract()
+    .body()
+    .as(PersonDTO.class, objectMapper);
+
+    person = createdPerson;
+
+    assertNotNull(createdPerson.getId());
+    assertTrue(createdPerson.getId() > 0);
+    assertNotNull(createdPerson.getFirstName());
+    assertNotNull(createdPerson.getLastName());
+    assertNotNull(createdPerson.getAddress());
+    assertNotNull(createdPerson.getGender());
+    assertFalse(createdPerson.getEnabled());
+    createdPerson.setLastName("Benedict Torvalds");
+    assertEquals("Linus",createdPerson.getFirstName());
+    assertEquals("Benedict Torvalds",createdPerson.getLastName());
+    assertEquals("Helsink - FINLAND",createdPerson.getAddress());
+    assertEquals("Male",createdPerson.getGender());
+    assertFalse(createdPerson.getEnabled());
+  }
+
+  @Test
+  @Order(5)
+  void delete() {
+    given(specification)
+    .accept(MediaType.APPLICATION_YAML_VALUE)
+    .pathParam("id", person.getId())
+    .when()
+    .delete("{id}")
+    .then()
+    .statusCode(204);    
+
+  }
+
+  @Test
+  @Order(6)
+  void findAll() throws JsonProcessingException {
+    var content = given().config(
+      RestAssured.config()
+      .encoderConfig(
+        EncoderConfig
+        .encoderConfig()
+        .encodeContentTypeAs(MediaType.APPLICATION_YAML_VALUE, ContentType.TEXT)
+      )
+    ).spec(specification)
+    .accept(MediaType.APPLICATION_YAML_VALUE)
+    .when()
+    .get()
+    .then()
+    .statusCode(200)
+    .contentType(MediaType.APPLICATION_YAML_VALUE)
+    .extract()
+    .body()
+    .as(PersonDTO[].class, objectMapper);
+
+    List<PersonDTO> people = Arrays.asList(content);
+    PersonDTO personOne = people.get(0);
+    person = personOne;
+
+    assertNotNull(personOne.getId());
+    assertTrue(personOne.getId() > 0);
+    assertNotNull(personOne.getFirstName());
+    assertNotNull(personOne.getLastName());
+    assertNotNull(personOne.getAddress());
+    assertNotNull(personOne.getGender());
+    assertTrue(personOne.getEnabled());
+
+    assertEquals("Ayrton",personOne.getFirstName());
+    assertEquals("Senna",personOne.getLastName());
+    assertEquals("São Paulo, Brazil",personOne.getAddress());
+    assertEquals("Male",personOne.getGender());
+    assertTrue(personOne.getEnabled());
+
+    PersonDTO personFour = people.get(4);
+    person = personFour;
+
+    assertNotNull(personFour.getId());
+    assertTrue(personFour.getId() > 0);
+    assertNotNull(personFour.getFirstName());
+    assertNotNull(personFour.getLastName());
+    assertNotNull(personFour.getAddress());
+    assertNotNull(personFour.getGender());
+    assertTrue(personFour.getEnabled());
+
+    assertEquals("Muhamad",personFour.getFirstName());
+    assertEquals("Ali",personFour.getLastName());
+    assertEquals("Kentuck - US",personFour.getAddress());
+    assertEquals("Male",personFour.getGender());
+    assertTrue(personFour.getEnabled());
+  }
+
+  private void mockPerson() {
+    person.setFirstName("Linus");
+    person.setLastName("Torvalds");
+    person.setAddress("Helsink - FINLAND");
+    person.setGender("Male");
+    person.setEnabled(true);
+  }
+}
---
+++ src/test/java/br/com/cristianoaf81/integrationtests/controllers/withyaml/mapper/YamlMapper.java
@@ -0,0 +1,44 @@
+package br.com.cristianoaf81.integrationtests.controllers.withyaml.mapper;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.dataformat.yaml.YAMLFactory;
+
+import io.restassured.mapper.ObjectMapper;
+import io.restassured.mapper.ObjectMapperDeserializationContext;
+import io.restassured.mapper.ObjectMapperSerializationContext;
+
+public class YamlMapper implements ObjectMapper {
+
+  private com.fasterxml.jackson.databind.ObjectMapper objectMapper;
+  protected TypeFactory typeFactory;
+
+
+  public YamlMapper() {
+    objectMapper = new com.fasterxml.jackson.databind.ObjectMapper(new YAMLFactory())
+      .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES);
+    typeFactory = TypeFactory.defaultInstance();
+  }
+
+  @Override
+  public Object serialize(ObjectMapperSerializationContext context) {
+    try {
+      return objectMapper.writeValueAsString(context.getObjectToSerialize());
+    } catch(JsonProcessingException e) {
+      throw new IllegalArgumentException("Error serializing yaml ", e);
+    }
+  }
+
+  @SuppressWarnings({"rawtypes"})
+  @Override
+  public Object deserialize(ObjectMapperDeserializationContext context) {
+    var data = context.getDataToDeserialize().asString();
+    Class type = (Class) context.getType();
+    try {
+      return objectMapper.readValue(data, typeFactory.constructType(type));    
+    } catch (JsonProcessingException e) {
+      throw new IllegalArgumentException("Error deserializing yaml ", e);
+    }
+  }
+}
