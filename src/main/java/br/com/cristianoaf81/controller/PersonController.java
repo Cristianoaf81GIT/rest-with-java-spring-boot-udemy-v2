e302b0a8835246e829bd535bfa8df508c2b01b10
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2026-04-20 13:02:05 -0300

    feat(187): Intgr. Servc. a factory xlsx Export.
    	modificar metodo addHateosLinks
    	adicionar metodo exportPage em PersonService
    
    Closes#csv-excel

--- src/main/java/br/com/cristianoaf81/controller/PersonController.java
+++ src/main/java/br/com/cristianoaf81/controller/PersonController.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.Sort;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PatchMapping;
@@ -23,6 +24,8 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.hateoas.EntityModel;
 import org.springframework.hateoas.PagedModel;
+import org.springframework.hateoas.server.core.LinkBuilderSupport;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 
 import br.com.cristianoaf81.controller.docs.PersonApiDocInterface;
 import br.com.cristianoaf81.dto.v1.PersonDTO;
@@ -36,149 +39,138 @@ public class PersonController implements PersonApiDocInterface {
 
   @Autowired
   private PersonService personService;
-   
-  // @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  // @CrossOrigin(origins = {"http://localhost:8080"})  // permite o controle granular de cors na aplicação
-  @GetMapping(
-    value = "/{id}", 
-    produces = { 
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
-      MediaType.APPLICATION_YAML_VALUE 
-    } 
-  )
+
+  // @RequestMapping(value = "/{id}", method = RequestMethod.GET, produces =
+  // MediaType.APPLICATION_JSON_VALUE)
+  // @CrossOrigin(origins = {"http://localhost:8080"}) // permite o controle
+  // granular de cors na aplicação
+  @GetMapping(value = "/{id}", produces = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
+      MediaType.APPLICATION_YAML_VALUE
+  })
   @Override
   public PersonDTO findById(@PathVariable(name = "id") Long id) {
     return personService.findById(id);
   }
-  
-  // @RequestMapping(method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
-  @GetMapping(
-    produces = { 
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
-      MediaType.APPLICATION_YAML_VALUE 
-    }//,
-   /* consumes = {  ****IMPORTANT GET METHOD DOES NOT CONSUMES MEDIA TYPE COMMENT IT*********
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
-      MediaType.APPLICATION_YAML_VALUE 
-    }*/
+
+  // @RequestMapping(method = RequestMethod.GET, produces =
+  // MediaType.APPLICATION_JSON_VALUE)
+  @GetMapping(produces = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
+      MediaType.APPLICATION_YAML_VALUE
+  }// ,
+  /*
+   * consumes = { ****IMPORTANT GET METHOD DOES NOT CONSUMES MEDIA TYPE COMMENT
+   * IT*********
+   * MediaType.APPLICATION_JSON_VALUE,
+   * MediaType.APPLICATION_XML_VALUE,
+   * MediaType.APPLICATION_YAML_VALUE
+   * }
+   */
   )
   @Override
   public ResponseEntity<PagedModel<EntityModel<PersonDTO>>> findAll(
-    @RequestParam(name = "page", defaultValue = "0") Integer page,
-    @RequestParam(name = "size", defaultValue = "12") Integer size,
-    @RequestParam(name = "direction", defaultValue = "asc") String direction
-  ) {
+      @RequestParam(name = "page", defaultValue = "0") Integer page,
+      @RequestParam(name = "size", defaultValue = "12") Integer size,
+      @RequestParam(name = "direction", defaultValue = "asc") String direction) {
     Direction sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
     Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
     return ResponseEntity.ok(personService.findAll(pageable));
   }
 
-  // @RequestMapping(method =  RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-  // @CrossOrigin(origins = {"http://localhost:8080"}) // libera apenas um metodo especifico do controller, pode ser usado também no controler inteiro
-  @PostMapping(
-    produces = {
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
-      MediaType.APPLICATION_YAML_VALUE 
-    }, 
-    consumes = {
-      MediaType.APPLICATION_JSON_VALUE, 
+  // @RequestMapping(method = RequestMethod.POST, produces =
+  // MediaType.APPLICATION_JSON_VALUE, consumes =
+  // MediaType.APPLICATION_JSON_VALUE)
+  // @CrossOrigin(origins = {"http://localhost:8080"}) // libera apenas um metodo
+  // especifico do controller, pode ser usado também no controler inteiro
+  @PostMapping(produces = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
+      MediaType.APPLICATION_YAML_VALUE
+  }, consumes = {
+      MediaType.APPLICATION_JSON_VALUE,
       MediaType.APPLICATION_YAML_VALUE,
       MediaType.APPLICATION_XML_VALUE
-    }
-  )
+  })
   @Override
   public PersonDTO create(@RequestBody PersonDTO person) {
     return personService.create(person);
   }
 
-  @PostMapping(
-    value = "/v2",
-    produces = {
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
+  @PostMapping(value = "/v2", produces = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
       MediaType.APPLICATION_YAML_VALUE
-    }, 
-    consumes = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_YAML_VALUE}
-  )
+  }, consumes = { MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_YAML_VALUE })
   public PersonDTOV2 createV2(@RequestBody PersonDTOV2 person) {
     return personService.createV2(person);
   }
 
-  // @RequestMapping(method = RequestMethod.PUT, produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-  @PutMapping(
-    produces = {
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
+  // @RequestMapping(method = RequestMethod.PUT, produces =
+  // MediaType.APPLICATION_JSON_VALUE, consumes =
+  // MediaType.APPLICATION_JSON_VALUE)
+  @PutMapping(produces = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
       MediaType.APPLICATION_YAML_VALUE
-    }, 
-    consumes = {
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
+  }, consumes = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
       MediaType.APPLICATION_YAML_VALUE
-    }
-  )
+  })
   @Override
   public PersonDTO update(@RequestBody PersonDTO person) {
     return personService.update(person);
-  }  
+  }
 
   // @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
   @DeleteMapping(value = "/{id}")
   @Override
-  public ResponseEntity<?> delete(@PathVariable(name="id") Long id) {
+  public ResponseEntity<?> delete(@PathVariable(name = "id") Long id) {
     return personService.delete(id);
   }
-  
 
-  @PatchMapping(
-    value = "/{id}", 
-    produces = { 
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
-      MediaType.APPLICATION_YAML_VALUE 
-    } 
-  )
+  @PatchMapping(value = "/{id}", produces = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
+      MediaType.APPLICATION_YAML_VALUE
+  })
   @Override
-  public PersonDTO disablePerson(@PathVariable(name="id") Long id) {
+  public PersonDTO disablePerson(@PathVariable(name = "id") Long id) {
     return personService.disablePerson(id);
   }
 
-
-  @GetMapping(
-    value = "/findPeopleByName/{firstName}",
-    produces = { 
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
-      MediaType.APPLICATION_YAML_VALUE 
-    }
-  )  
+  @GetMapping(value = "/findPeopleByName/{firstName}", produces = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
+      MediaType.APPLICATION_YAML_VALUE
+  })
   @Override
   public ResponseEntity<PagedModel<EntityModel<PersonDTO>>> findByName(
-    @PathVariable(name = "firstName") String firstName,
-    @RequestParam(name = "page", defaultValue = "0") Integer page,
-    @RequestParam(name = "size", defaultValue = "12") Integer size,
-    @RequestParam(name = "direction", defaultValue = "asc") String direction
-  ) {
+      @PathVariable(name = "firstName") String firstName,
+      @RequestParam(name = "page", defaultValue = "0") Integer page,
+      @RequestParam(name = "size", defaultValue = "12") Integer size,
+      @RequestParam(name = "direction", defaultValue = "asc") String direction) {
     Direction sortDirection = "desc".equalsIgnoreCase(direction) ? Direction.DESC : Direction.ASC;
     Pageable pageable = PageRequest.of(page, size, Sort.by(sortDirection, "firstName"));
 
-    return ResponseEntity.ok(personService.findByName(firstName, pageable)); 
+    return ResponseEntity.ok(personService.findByName(firstName, pageable));
   }
-  
-  @PostMapping(
-    value="/massCreation",
-    produces = { 
-      MediaType.APPLICATION_JSON_VALUE, 
-      MediaType.APPLICATION_XML_VALUE, 
-      MediaType.APPLICATION_YAML_VALUE 
-    }
-  )
+
+  @PostMapping(value = "/massCreation", produces = {
+      MediaType.APPLICATION_JSON_VALUE,
+      MediaType.APPLICATION_XML_VALUE,
+      MediaType.APPLICATION_YAML_VALUE
+  })
   @Override
   public List<PersonDTO> massCreation(@RequestParam(name = "file") MultipartFile file) {
     return personService.massCreation(file);
+  }
+
+  public Resource exportPage(int i, int j, String string, String applicationXlsxValue) {
+    // TODO Auto-generated method stub
+    return null;
   }
 }
--- src/main/java/br/com/cristianoaf81/services/person/PersonService.java
+++ src/main/java/br/com/cristianoaf81/services/person/PersonService.java
@@ -11,6 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
@@ -29,6 +30,9 @@ import br.com.cristianoaf81.exception.BadRequestException;
 import br.com.cristianoaf81.exception.FileStorageException;
 import br.com.cristianoaf81.exception.RequiredObjectIsNullException;
 import br.com.cristianoaf81.exception.ResourceNotFoundException;
+import br.com.cristianoaf81.file.exporter.MediaTypes;
+import br.com.cristianoaf81.file.exporter.contract.FileExporter;
+import br.com.cristianoaf81.file.exporter.factory.FileExporterFactory;
 import br.com.cristianoaf81.file.importer.contract.FileImporter;
 import br.com.cristianoaf81.file.importer.factory.FileImporterFactory;
 import br.com.cristianoaf81.mapper.custom.PersonMapper;
@@ -43,7 +47,7 @@ import br.com.cristianoaf81.dto.v2.PersonDTOV2;
 
 @Service
 public class PersonService {
-    
+
   private AtomicLong counter = new AtomicLong();
 
   private Logger logger = LoggerFactory.getLogger(getClass());
@@ -60,8 +64,10 @@ public class PersonService {
   @Autowired
   private FileImporterFactory fileImporterFactory;
 
+  @Autowired
+  private FileExporterFactory fileExporter;
 
-  List<PersonDTO> persons = new ArrayList<PersonDTO>();  
+  List<PersonDTO> persons = new ArrayList<PersonDTO>();
 
   public PersonService() {
     for (int i = 0; i < 8; i++) {
@@ -75,45 +81,45 @@ public class PersonService {
     logger.info("Finding one person!");
     String errMsg = String.format("No record found for this id: [%s]", id);
     var dto = parseObject(repository
-      .findById(id)
-      .orElseThrow(() -> new ResourceNotFoundException(errMsg)), PersonDTO.class);
+        .findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(errMsg)), PersonDTO.class);
     dto.add(
-      linkTo(
-        methodOn(PersonController.class).findById(id)
-      ).withSelfRel().withType("GET"));
+        linkTo(
+            methodOn(PersonController.class).findById(id)).withSelfRel().withType("GET"));
     dto.setId(id);
     addHateosLinks(dto);
     return dto;
-  } 
- 
+  }
+
   public PagedModel<EntityModel<PersonDTO>> findAll(Pageable pageable) {
     logger.info("Finding all people");
 
     var people = repository.findAll(pageable);
-    return buildPagedModel(pageable, people);  
+    return buildPagedModel(pageable, people);
   }
-  
+
   @Transactional
   public PagedModel<EntityModel<PersonDTO>> findByName(String firstName, Pageable pageable) {
     logger.info("Finding by name");
 
-    var people = repository.findPeopleByName(firstName,pageable);
-    return buildPagedModel(pageable, people);  
+    var people = repository.findPeopleByName(firstName, pageable);
+    return buildPagedModel(pageable, people);
   }
 
   private Person mockPerson(int i) {
     Person person = new Person();
     person.setId(counter.incrementAndGet());
-    person.setFirstName(String.format("PERSON_NAME_%d", i+1));
-    person.setLastName(String.format("PERSON_LAST_NAME_%d", i+1));
-    person.setAddress(String.format("ADDRESS_%d", i+1));
+    person.setFirstName(String.format("PERSON_NAME_%d", i + 1));
+    person.setLastName(String.format("PERSON_LAST_NAME_%d", i + 1));
+    person.setAddress(String.format("ADDRESS_%d", i + 1));
     String gender = i % 2 == 0 ? "MALE" : "FEMALE";
     person.setGender(gender);
     return person;
   }
 
   public PersonDTO create(PersonDTO person) {
-    if (person == null) throw new RequiredObjectIsNullException();
+    if (person == null)
+      throw new RequiredObjectIsNullException();
     logger.info("Creating one person");
     Person p = parseObject(person, Person.class);
     PersonDTO dto = parseObject(repository.save(p), PersonDTO.class);
@@ -121,25 +127,25 @@ public class PersonService {
     return dto;
   }
 
-
   public PersonDTOV2 createV2(PersonDTOV2 person) {
-    if (person == null) throw new RequiredObjectIsNullException();
+    if (person == null)
+      throw new RequiredObjectIsNullException();
     logger.info("Creating one personV2" + person);
     Person p = converter.convertDTOtoEntity(person);
     logger.info("[person] " + p);
     return converter.convertEntityToDTO(repository.save(p));
   }
 
-
   public PersonDTO update(PersonDTO person) {
-    if (person == null) throw new RequiredObjectIsNullException();
+    if (person == null)
+      throw new RequiredObjectIsNullException();
     logger.info("Updating one person");
     String errMsg = String.format("No record found for this person id: [%s]", person.getId());
-    
+
     Person existingPerson = repository
-      .findById(person.getId())
-      .orElseThrow(() -> new ResourceNotFoundException(errMsg));
-   
+        .findById(person.getId())
+        .orElseThrow(() -> new ResourceNotFoundException(errMsg));
+
     if (person.getGender() != null && person.getGender().length() > 0)
       existingPerson.setGender(person.getGender());
     if (person.getAddress() != null && person.getAddress().length() > 0)
@@ -148,7 +154,7 @@ public class PersonService {
       existingPerson.setLastName(person.getLastName());
     if (person.getFirstName() != null && person.getLastName().length() > 0)
       existingPerson.setFirstName(person.getFirstName());
-    
+
     PersonDTO dto = parseObject(repository.save(existingPerson), PersonDTO.class);
     addHateosLinks(dto);
     return dto;
@@ -158,22 +164,22 @@ public class PersonService {
     logger.info(String.format("Deleting person with id %s", id));
     String errMsg = String.format("No record found for this person id: [%s]", id);
     Person existingPerson = repository
-      .findById(id)
-      .orElseThrow(() -> new ResourceNotFoundException(errMsg));
+        .findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(errMsg));
 
     if (existingPerson != null)
       repository.delete(existingPerson);
 
     return ResponseEntity.noContent().build();
   }
- 
-  @Transactional // necessario quando implementamos um metodo nao default jpq no repository 
+
+  @Transactional // necessario quando implementamos um metodo nao default jpq no repository
   public PersonDTO disablePerson(Long id) {
     logger.info(String.format("Disabling person with id %s", id));
     String errMsg = String.format("No record found for this person id: [%s]", id);
     repository
-      .findById(id)
-      .orElseThrow(() -> new ResourceNotFoundException(errMsg));
+        .findById(id)
+        .orElseThrow(() -> new ResourceNotFoundException(errMsg));
 
     repository.disablePerson(id); // metodo nao default implementado no repository do jpa
     var entity = repository.findById(id).get();
@@ -184,48 +190,43 @@ public class PersonService {
 
   public void addHateosLinks(PersonDTO dto) {
     dto.add(
-      linkTo(
-        methodOn(PersonController.class).findById(dto.getId())
-      ).withSelfRel().withType("GET"));
+        linkTo(
+            methodOn(PersonController.class).findById(dto.getId())).withSelfRel().withType("GET"));
 
     dto.add(
-      linkTo(
-        methodOn(PersonController.class).findByName("",1,12,"asc")
-      ).withRel("findByName").withType("GET"));
-
+        linkTo(
+            methodOn(PersonController.class).findByName("", 1, 12, "asc")).withRel("findByName").withType("GET"));
 
     dto.add(
-      linkTo(
-        methodOn(PersonController.class).findAll(1,12, "asc")
-      ).withRel("findAll").withType("GET"));
+        linkTo(
+            methodOn(PersonController.class).findAll(1, 12, "asc")).withRel("findAll").withType("GET"));
 
     dto.add(
-      linkTo(
-        methodOn(PersonController.class).delete(dto.getId())
-      ).withRel("delete").withType("DELETE"));
+        linkTo(
+            methodOn(PersonController.class).delete(dto.getId())).withRel("delete").withType("DELETE"));
 
-  
     dto.add(
-      linkTo(
-        methodOn(PersonController.class).create(dto)
-      ).withRel("create").withType("POST"));
+        linkTo(
+            methodOn(PersonController.class).create(dto)).withRel("create").withType("POST"));
 
     dto.add(
         linkTo(methodOn(PersonController.class).massCreation(null))
-        .withRel("massCreation")
-        .withType("POST")
-    );
+            .withRel("massCreation")
+            .withType("POST"));
 
     dto.add(
-      linkTo(
-        methodOn(PersonController.class).update(dto)
-      ).withRel("update").withType("PUT"));
+        linkTo(
+            methodOn(PersonController.class).update(dto)).withRel("update").withType("PUT"));
 
     dto.add(
-      linkTo(
-        methodOn(PersonController.class).disablePerson(dto.getId())
-      ).withRel("disable").withType("PATCH")
-    );
+        linkTo(
+            methodOn(PersonController.class).disablePerson(dto.getId())).withRel("disable").withType("PATCH"));
+
+    dto.add(
+        linkTo(
+            methodOn(PersonController.class).exportPage(1, 12, "asc", MediaTypes.APPLICATION_XLSX_VALUE))
+            .withRel("exportPage").withType("GET"));
+
   }
 
   private PagedModel<EntityModel<PersonDTO>> buildPagedModel(Pageable pageable, Page<Person> people) {
@@ -235,36 +236,37 @@ public class PersonService {
       addHateosLinks(dto);
       return dto;
     });
-    
-    Link findAllLink = WebMvcLinkBuilder
-    .linkTo(WebMvcLinkBuilder.methodOn(PersonController.class)
-      .findAll(pageable.getPageNumber(), pageable.getPageSize(), String.valueOf(pageable.getSort()))).withSelfRel();
 
-    return assembler.toModel(peopleWithLinks,findAllLink);
+    Link findAllLink = WebMvcLinkBuilder
+        .linkTo(WebMvcLinkBuilder.methodOn(PersonController.class)
+            .findAll(pageable.getPageNumber(), pageable.getPageSize(), String.valueOf(pageable.getSort())))
+        .withSelfRel();
+
+    return assembler.toModel(peopleWithLinks, findAllLink);
   }
 
   public List<PersonDTO> massCreation(MultipartFile file) {
     logger.info("Importing people from file!");
 
-    if (file.isEmpty()) throw new BadRequestException("Please set a valid file!");
+    if (file.isEmpty())
+      throw new BadRequestException("Please set a valid file!");
 
-    try(InputStream is = file.getInputStream()) {
+    try (InputStream is = file.getInputStream()) {
       String fileName = Optional.ofNullable(file.getOriginalFilename())
-      .orElseThrow(() -> new BadRequestException("File name cannot be null"));
+          .orElseThrow(() -> new BadRequestException("File name cannot be null"));
 
       FileImporter importer = this.fileImporterFactory.getImporter(fileName);
       List<Person> entities = importer.importFile(is)
-        .stream()
-        .map((PersonDTO dto) -> repository.save(parseObject(dto, Person.class)))
-        .collect(Collectors.toList());
-
+          .stream()
+          .map((PersonDTO dto) -> repository.save(parseObject(dto, Person.class)))
+          .collect(Collectors.toList());
 
       var peopleWithLinks = entities.stream().map(p -> {
         var dto = parseObject(p, PersonDTO.class);
         addHateosLinks(dto);
         return dto;
       });
-      
+
       return peopleWithLinks.toList();
     } catch (Exception e) {
       String message = String.format("Error while trying to import file [%s]", e);
@@ -272,6 +274,21 @@ public class PersonService {
       throw new FileStorageException("Error processing file!");
     }
 
+  }
+
+  public Resource exportPage(Pageable pageable, String acceptHeader) {
+    logger.info("Exporting a people page");
+
+    var people = repository.findAll(pageable)
+        .map(person -> parseObject(person, PersonDTO.class))
+        .getContent();
+
+    try {
+      FileExporter exporter = fileExporter.getExporter(acceptHeader);
+      return exporter.ExportFile(people);
+    } catch (Exception e) {
+      throw new RuntimeException("Error during file export!", e);
+    }
   }
 
 }
