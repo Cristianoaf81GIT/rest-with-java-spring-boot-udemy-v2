16c7e6ddf27909fbc938ce05e246e30a5bb0d45f
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2026-08-03 14:17:10 -0300

    criando met service para email com attach

--- src/main/java/br/com/cristianoaf81/services/EmailService.java
+++ src/main/java/br/com/cristianoaf81/services/EmailService.java
@@ -1,7 +1,14 @@
 package br.com.cristianoaf81.services;
 
+import java.io.File;
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 import br.com.cristianoaf81.config.EmailConfig;
 import br.com.cristianoaf81.dto.request.EmailRequestDTO;
@@ -22,6 +29,29 @@ public class EmailService {
         .withSubject(emailRequestDTO.getSubject())
         .withBodyMessage(emailRequestDTO.getBody())
         .send(emailConfig);
+  }
+
+  public void sendEmailWithAttachment(String emailRequestJson, MultipartFile attachment) {
+    File tempFile = null;
+    try {
+      EmailRequestDTO emailRequestDTO = new ObjectMapper().readValue(emailRequestJson, EmailRequestDTO.class);
+      tempFile = File.createTempFile("attachment", attachment.getOriginalFilename());
+      attachment.transferTo(tempFile);
+      emailSender
+          .to(emailRequestDTO.getTo())
+          .withSubject(emailRequestDTO.getSubject())
+          .withBodyMessage(emailRequestDTO.getSubject())
+          .attach(tempFile.getAbsolutePath())
+          .send(emailConfig);
+    } catch (JsonProcessingException e) {
+      throw new RuntimeException("Error parsing email request JSON", e);
+    } catch (IOException e) {
+      throw new RuntimeException("Error creating temporary file for attachment", e);
+    } finally {
+      if (tempFile != null && tempFile.exists()) {
+        tempFile.delete();
+      }
+    }
   }
 
 }
