d9dd267d9ee44e6a11dc22df4a06c3df5d3a3912
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2026-08-12 14:08:11 -0300

    feat(241): impl. user repo e user serv.
    criando classe UserRepository.java
    criando classe userService.java

---
+++ src/main/java/br/com/cristianoaf81/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package br.com.cristianoaf81.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import br.com.cristianoaf81.model.User;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+  @Query("SELECT u FROM User u WHERE u.userName =: userName")
+  User findByUserName(@Param("userName") String userName);
+}
---
+++ src/main/java/br/com/cristianoaf81/services/user/UserService.java
@@ -0,0 +1,32 @@
+package br.com.cristianoaf81.services.user;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import br.com.cristianoaf81.repository.UserRepository;
+
+public class UserService implements UserDetailsService {
+
+  // injeção via propriedade é mais flexível, mas com mais chaces de null pointer
+  @Autowired
+  UserRepository repository;
+
+  // injeção via constructor causa mais acoplamento, mas a aplicação é forçada a
+  // iniciar com as dependencias já instanciadas
+  public UserService(UserRepository repository) {
+    this.repository = repository;
+  }
+
+  @Override
+  public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+    var user = repository.findByUserName(username);
+    if (user != null) {
+      return user;
+    } else {
+      throw new UsernameNotFoundException("Username {" + username + "} not found:");
+    }
+  }
+
+}
