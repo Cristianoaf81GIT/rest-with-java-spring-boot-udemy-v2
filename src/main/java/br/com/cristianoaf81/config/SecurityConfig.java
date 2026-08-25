1e817df32757d8d9efa87b048c5be09375b69a09
Cristiano Alexandre de Faria <cristianoaf81@hotmail.com>
2026-08-25 12:12:34 -0300

    feat(247): Implementando AuthenticationManager e SecurityFilterChain

--- src/main/java/br/com/cristianoaf81/config/SecurityConfig.java
+++ src/main/java/br/com/cristianoaf81/config/SecurityConfig.java
@@ -6,12 +6,20 @@ import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder;
 import org.springframework.security.crypto.password.Pbkdf2PasswordEncoder.SecretKeyFactoryAlgorithm;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import br.com.cristianoaf81.security.jwt.JwtTokenFilter;
 import br.com.cristianoaf81.security.jwt.JwtTokenProvider;
 
 @EnableWebSecurity
@@ -37,5 +45,35 @@ public class SecurityConfig {
     DelegatingPasswordEncoder passwordEncoder = new DelegatingPasswordEncoder("pbkdf2", encoders);
     passwordEncoder.setDefaultPasswordEncoderForMatches(pbkdf2Encoder);
     return passwordEncoder;
+  }
+
+  @Bean
+  AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+    return configuration.getAuthenticationManager();
+  }
+
+  SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    JwtTokenFilter filter = new JwtTokenFilter(jwtTokenProvider);
+    //@formatter:off
+    return http
+      .httpBasic(AbstractHttpConfigurer::disable) // desabilita basic auth (user and password via Pbkdf2PasswordEncoder)
+      .csrf(AbstractHttpConfigurer::disable) // desabilita o cross site request forgery
+      .addFilterBefore(filter, UsernamePasswordAuthenticationFilter.class)
+      .sessionManagement((session) -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+      .authorizeHttpRequests((authorizeHttpRequests) -> 
+          authorizeHttpRequests.requestMatchers(
+            "/auth/signin",
+            "/auth/refresh/**",
+            "/auth/createUser", // remover após uso em ambiente de desenvolvimento (remover em produção)
+            "/swagger-ui/**",
+            "/v3/api-docs/**"
+          ).permitAll()
+          .requestMatchers(
+            "/api/**"
+          ).authenticated()
+          .requestMatchers("/users").denyAll() // bloqueia acesso via jpa a endpoints de usuários
+      ).cors((cors) -> {})
+      .build();
+    //@formatter:on
   }
 }
